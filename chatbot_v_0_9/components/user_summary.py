13e1fb0d29f5679e82c920eef9f4d2e37602d78d
Andreas Feik <feik.andi@gmail.com>
2024-11-20 22:01:13 +0100

    analytics

--- chatbot_v_0_9/components/user_summary.py
+++ chatbot_v_0_9/components/user_summary.py
@@ -2,6 +2,7 @@ import streamlit as st
 from .footnote import write_footnote
 from .db_communication import insert_db_message
 from .utils import get_chatbot_config, language_dropdown,send_ga_event
+import streamlit_analytics
 
 chatbot_config = get_chatbot_config()
 
@@ -54,61 +55,62 @@ def get_user_statement_and_summary(client):
         min_char_count = 30
         char_count = len(statement)  # Using len(statement) as in the initial code
 
-        if submit_button:
-            can_submit = char_count >= min_char_count
-            if not can_submit:
-                send_ga_event("initial_statement_too_few_characters")
-                # Display the error message in the second column, next to the button
-                with col2:
-                    error_placeholder.markdown(
-                        _(
-                            """<div style="background-color: #000000; color: gray; font-size: 13px; padding: 5px; text-align: center; border-radius: 5px;">
-                            Please enter at least """
-                        ) + str(min_char_count) + _(
-                            """ characters. You currently have: """
-                        ) + str(char_count) + _(""" characters.</div>"""),
-                        unsafe_allow_html=True
-                    )
-            else:
-                # Clear the error message if any
-                send_ga_event("initial_statement_submitted")
-                error_placeholder.empty()
-                if st.session_state.lang == "de":
-                    lang_prompt = "Use German"
+        with streamlit_analytics.track():
+            if submit_button:
+                can_submit = char_count >= min_char_count
+                if not can_submit:
+                    send_ga_event("initial_statement_too_few_characters")
+                    # Display the error message in the second column, next to the button
+                    with col2:
+                        error_placeholder.markdown(
+                            _(
+                                """<div style="background-color: #000000; color: gray; font-size: 13px; padding: 5px; text-align: center; border-radius: 5px;">
+                                Please enter at least """
+                            ) + str(min_char_count) + _(
+                                """ characters. You currently have: """
+                            ) + str(char_count) + _(""" characters.</div>"""),
+                            unsafe_allow_html=True
+                        )
                 else:
-                    lang_prompt = "Use English"
+                    # Clear the error message if any
+                    send_ga_event("initial_statement_submitted")
+                    error_placeholder.empty()
+                    if st.session_state.lang == "de":
+                        lang_prompt = "Use German"
+                    else:
+                        lang_prompt = "Use English"
 
-                summary_response = client.messages.create(
-                    model="claude-3-5-sonnet-20241022",
-                    max_tokens=chatbot_config[st.session_state.proficiency][
-                        "summary_max_tokens"
-                    ],
-                    system=lang_prompt
-                    + chatbot_config[st.session_state.proficiency]["summary_role"],
-                    messages=[
-                        {
-                            "role": "user",
-                            "content": [{"type": "text", "text": statement}],
-                        }
-                    ],
-                    temperature=chatbot_config[st.session_state.proficiency][
-                        "summary_temperature"
-                    ],
-                )
-                placeholder.empty()
-                summary = summary_response.content[0].text.strip()
-                insert_db_message(
-                    statement, role="user", message_type="initial_statement"
-                )
-                insert_db_message(
-                    summary, role="assistant", message_type="initial_statement_summary"
-                )
+                    summary_response = client.messages.create(
+                        model="claude-3-5-sonnet-20241022",
+                        max_tokens=chatbot_config[st.session_state.proficiency][
+                            "summary_max_tokens"
+                        ],
+                        system=lang_prompt
+                        + chatbot_config[st.session_state.proficiency]["summary_role"],
+                        messages=[
+                            {
+                                "role": "user",
+                                "content": [{"type": "text", "text": statement}],
+                            }
+                        ],
+                        temperature=chatbot_config[st.session_state.proficiency][
+                            "summary_temperature"
+                        ],
+                    )
+                    placeholder.empty()
+                    summary = summary_response.content[0].text.strip()
+                    insert_db_message(
+                        statement, role="user", message_type="initial_statement"
+                    )
+                    insert_db_message(
+                        summary, role="assistant", message_type="initial_statement_summary"
+                    )
 
-                st.session_state.summary = summary
-                st.session_state.statement = statement
-                st.session_state.step = "initial_rating"
-                
-                st.rerun()
+                    st.session_state.summary = summary
+                    st.session_state.statement = statement
+                    st.session_state.step = "initial_rating"
+                    
+                    st.rerun()
                 
 
     write_footnote()
